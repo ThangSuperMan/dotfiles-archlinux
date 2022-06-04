85d7b9c1b69cfc28743225889d742d1367d283c8
thangsuperman <thangjenny2002@gmail.com>
2022-06-05 00:11:20 +0700

    Adding the new style for the polybar bar

--- .config/polybar/scripts/ibus.sh
+++ .config/polybar/scripts/ibus.sh
@@ -4,7 +4,4 @@ if [[ `ibus engine | awk -F":" '{ print $2 }'` == "us" ]]; then
 	ibus engine 'Bamboo::Candy'
 else
 	ibus engine 'xkb:us::eng'
-
-	# Reload the keybinding for keyboard
-	xmodmap ~/.Xmodmap
 fi
